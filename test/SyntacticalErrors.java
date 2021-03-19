98c02f45daf57f5be3337079a5bd09b948e6d375
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2021-03-19 17:25:01 -0100

    Syntatical test done

--- test/SyntacticalErrors.java
+++ test/SyntacticalErrors.java
@@ -5,4 +5,25 @@ import pt.up.fe.comp.jmm.JmmParserResult;
 import static org.junit.Assert.*;
 
 public class SyntacticalErrors {
+
+    @Test
+    public void BlowUp() {
+        JmmParserResult res = TestUtils.parse("test/fixtures/public/fail/syntactical/BlowUp.jmm");
+        assertEquals("Program",res.getRootNode().getKind());
+        assertEquals(4, TestUtils.getNumErrors(res.getReports()));
+    }
+
+    @Test
+    public void CompleteWhileTest() {
+        JmmParserResult res = TestUtils.parse("test/fixtures/public/fail/syntactical/CompleteWhileTest.jmm");
+        assertEquals("Program",res.getRootNode().getKind());
+        assertEquals(4, TestUtils.getNumErrors(res.getReports()));          // Não sei qual o numero
+    }
+
+    @Test
+    public void MissingRightPar() {
+        JmmParserResult res = TestUtils.parse("test/fixtures/public/fail/syntactical/MissingRightPar.jmm");
+        assertEquals("Program",res.getRootNode().getKind());
+        assertEquals(1, TestUtils.getNumErrors(res.getReports()));
+    }
 }
