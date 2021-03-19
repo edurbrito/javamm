3060dcec54682393747468a1bf74c043dd7014f4
LuisMMMTS <torres.luismiguel@gmail.com>
2021-03-19 18:25:42 +0000

    added syntactical tests failing

--- test/SyntacticalErrors.java
+++ test/SyntacticalErrors.java
@@ -5,4 +5,31 @@ import pt.up.fe.comp.jmm.JmmParserResult;
 import static org.junit.Assert.*;
 
 public class SyntacticalErrors {
+
+    @Test
+    public void MissingRightPar() {
+        // Ler de ficheiro
+
+        JmmParserResult res = TestUtils.parse("test/fixtures/public/fail/syntactical/MissingRightPar.jmm");
+        assertEquals("Program",res.getRootNode().getKind());
+        assertEquals(TestUtils.getNumErrors(res.getReports()),1);
+    }
+
+    @Test
+    public void MultipleSequential() {
+        // Ler de ficheiro
+
+        JmmParserResult res = TestUtils.parse("test/fixtures/public/fail/syntactical/MultipleSequential.jmm");
+        assertEquals("Program",res.getRootNode().getKind());
+        assertEquals(TestUtils.getNumErrors(res.getReports()),2);
+    }
+
+    @Test
+    public void NestedLoop() {
+        // Ler de ficheiro
+
+        JmmParserResult res = TestUtils.parse("test/fixtures/public/fail/syntactical/NestedLoop.jmm");
+        assertEquals("Program",res.getRootNode().getKind());
+        assertEquals(TestUtils.getNumErrors(res.getReports()),2);
+    }
 }
