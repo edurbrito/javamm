662f21c3cf057b977cb98f8e33d0de425c6de166
LuisMMMTS <torres.luismiguel@gmail.com>
2021-03-19 17:24:32 +0000

    added tests

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -9,6 +9,27 @@ public class ExampleTest {
     @Test
     public void testExpression() {
         // Ler de ficheiro
-        // assertEquals("Parse", TestUtils.parse("test/fixtures/public/HelloWorld.jmm").getRootNode().getKind());
+        assertEquals("Parse", TestUtils.parse("test/fixtures/public/Array1.jmm").getRootNode().getKind());
+        assertEquals("Parse", TestUtils.parse("test/fixtures/public/Array2.jmm").getRootNode().getKind());
+        assertEquals("Parse", TestUtils.parse("test/fixtures/public/Array3.jmm").getRootNode().getKind());
+        assertEquals("Parse", TestUtils.parse("test/fixtures/public/Custom.jmm").getRootNode().getKind());
+        assertEquals("Parse", TestUtils.parse("test/fixtures/public/FindMaximum.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/HelloWorld.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/Lazysort.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/Lif.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/MonteCarloPi.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/notInitInIf.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/notInitInIf2.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/notInitInIf3.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/notInitInIf4.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/QuickSort.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/Simple.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/test2.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/test3.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/test4.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/test5.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/TicTacToe.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/varNotInit.jmm").getRootNode().getKind());
+         assertEquals("Parse", TestUtils.parse("test/fixtures/public/WhileAndIF.jmm").getRootNode().getKind());
 	}
 }
